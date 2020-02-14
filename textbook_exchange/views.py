40f89365133748aaf86ae88b8be81e2a1778c0c7
zmeherin <zmeherin@gmail.com>
2020-02-14 03:13:58 -0500

    added base template

--- textbook_exchange/views.py
+++ textbook_exchange/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    return render(request, 'textbook_exchange/landing.html')
+
+def buy_books(request):
+    return render(request, 'textbook_exchange/buybooks.html', {'title': 'Buy Books'})
--- textexc/settings.py
+++ textexc/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'textbook_exchange.apps.TextbookExchangeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
