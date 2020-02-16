5978bff18b354cd995ccb09bf3eab88f90b72497
Nick Winans <nw5zp@virginia.edu>
2020-02-16 18:19:57 -0500

    updated templates to properly extend, added django style links

--- textbook_exchange/urls.py
+++ textbook_exchange/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "exchange"
+
 urlpatterns = [
     path('', views.landing, name='landing'),
     path('buy/', views.buy_books, name='buybooks'),
