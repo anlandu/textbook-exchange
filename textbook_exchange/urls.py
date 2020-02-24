f6a06735080ddd5feb98a202cdd13a750c145b3f
Rohan Chandra <rc8yw@virginia.edu>
2020-02-24 14:48:44 -0500

    Started the html for the account page, added a simple view and its url

--- textbook_exchange/urls.py
+++ textbook_exchange/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('buy/', views.buy_books, name='buybooks'),
     path('sell/', views.sell_books, name='sellbooks')
+    path('account/', views.account_page, anme='accountpage')
 ]
