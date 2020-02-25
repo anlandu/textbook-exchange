6c80922138001f28950ead52680c97bbe6a58900
zmeherin <zmeherin@gmail.com>
2020-02-24 23:20:08 -0500

    removed empty args in CharField

--- textbook_exchange/models.py
+++ textbook_exchange/models.py
@@ -1,18 +1,26 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
-from django.db import models
+from django.db import models, Field
 from django.utils import timezone
 
 
 # Create your models here.
 #Django User Model
 class User(AbstractUser):
-    username = models.CharField(_('username'), max_length = 30, unique = True)
-    first_name = models.CharField(_('first name'), max_length = 30, blank = True)
-    last_name = models.CharField(_('last name'), max_length = 30, blank = True)
-    email = models.EmailField(_('email address'), blank = True)
-    password = models.CharField(_('password'), max_length = 30)
-    is_staff = models.BooleanField(_('staff status'), default = False)
-    date_joined = models.DateTimeField(_('date joined'), default = timezone.now)
+    # username = models.CharField(_('username'), max_length = 30, unique = True)
+    # first_name = models.CharField(_('first name'), max_length = 30, blank = True)
+    # last_name = models.CharField(_('last name'), max_length = 30, blank = True)
+    # email = models.EmailField(_('email address'), blank = True)
+    # password = models.CharField(_('password'), max_length = 30)
+    # is_staff = models.BooleanField(_('staff status'), default = False)
+    # date_joined = models.DateTimeField(_('date joined'), default = timezone.now)
+
+    username = models.CharField(max_length = 30, unique = True)
+    first_name = models.CharField(max_length = 30, blank = True)
+    last_name = models.CharField(max_length = 30, blank = True)
+    email = models.EmailField(, blank = True)
+    password = models.CharField(max_length = 30)
+    is_staff = models.BooleanField(default = False)
+    date_joined = models.DateTimeField(default = timezone.now)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
@@ -44,10 +52,11 @@ class Class(models.Model):
         return class_info.strip()
 
 class Listing(models.Model):
-#    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    # user = models.ForeignKey(User, on_delete = models.CASCADE)
     textbook = models.ForeignKey(Textbook, on_delete = models.CASCADE)
     class_object = models.ForeignKey(Class, on_delete = models.CASCADE)
-    condition = models.IntegerField(default = 0)
+    # condition = models.IntegerField(default = 0)
+    condition = Field.choices[("likenew", "Like new"), ("verygood", "Very good"), ("good", "Good"), ("acceptable", "Acceptable")]
     picture = models.ImageField(upload_to = 'listing_images')
     comments = ArrayField(models.CharField(max_length = 500), blank = True)
     price = models.DecimalField(max_digits = 5, decimal_places = 2)
