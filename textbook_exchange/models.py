75d32b8566b4fef6ebe3a38c3a6a78f9f90d2723
zmeherin <zmeherin@gmail.com>
2020-02-24 23:05:55 -0500

    changed import AbstractBaseUser to AbstractUser

--- textbook_exchange/models.py
+++ textbook_exchange/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 
