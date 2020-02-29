16b07bdd4b7c62ed47679592ae1d72d03facc130
zmeherin <zmeherin@gmail.com>
2020-02-29 15:35:44 -0500

    cleaned up

--- textbook_exchange/templatetags/listing_extras.py
+++ textbook_exchange/templatetags/listing_extras.py
@@ -1,13 +1,10 @@
 from django import template
 register = template.Library()
 
+# You can pass arguments to your custom tag, process them a bit in Python, then bounce back to a template. 
+# Direct inclusion only works for fragments that don't depend on the surrounding context.
+
 @register.inclusion_tag('product_listing.html')
-def show_product_listings(textbook_exchange): # is poll here supposed to be the model ProductListing?
+def show_product_listings(textbook_exchange):
     product_listings = textbook_exchange.product_listing_set.all() # what is polls.choice_set.all() replaced by?
     return {'product_listings': product_listings}
-
-
-# @register.inclusion_tag('textbook_listing.html')
-# def show_product_listings(textbook_exchange): # is poll here supposed to be the model ProductListing?
-#     textbooks = textbook_exchange.textbook_set.all() # what is polls.choice_set.all() replaced by?
-#     return {'textbooks': textbooks}
