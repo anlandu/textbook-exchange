d157abac9d9edb64e2b0483f88d683b0a6ae2635
Rohan Chandra <rc8yw@virginia.edu>
2020-03-09 16:52:39 -0400

    Added a static files folder to alleviate an error about accessing a page. Added a couple more tests, though some aren't working as intended.

--- textbook_exchange/tests.py
+++ textbook_exchange/tests.py
@@ -5,6 +5,10 @@ from django.utils import timezone
 from django.core.exceptions import ValidationError
 from textbook_exchange.forms import *
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files import File
+from django.urls import *
+from . import *
+from django.test import Client
 
 
 class HelloWord(TestCase):
@@ -42,6 +46,7 @@ class AutocompleteTest(TestCase):
             str(response.content, encoding='utf8'),
             expected_results
         )
+
 class LoginTest(TestCase):
     def test_login_success(self):
         test_user = User.objects.create(
@@ -57,6 +62,7 @@ class LoginTest(TestCase):
             'logged_in': self.client.login(username = test_user.email)
         }
         self.assertTrue(context)
+
 class UserModelTest(TestCase):
     def test_user_no_edge_cases(self):
         test_user = User.objects.create(
@@ -103,18 +109,42 @@ class UserModelTest(TestCase):
         test_user.username = 1.456
         test_user.save()
         self.assertEqual(type(test_user.username), float) #Originally was str instead of float
+    def test_name_and_email_on_account_page(self):
+        test_user = User(
+            username = "rc8yw",
+            password = "12345",
+            first_name = "Rohan",
+            last_name = "Chandra",
+            email = "rc8yw@virginia.edu",
+            is_staff = False,
+            date_joined = timezone.now()
+        )
+        test_user.save()
+        c = Client()
+        logged_in = c.login(username = test_user.email)
+        self.assertTrue(test_user.is_authenticated)
+        response = self.client.get('/accounts/', secure = True, follow = True)
+        self.assertEqual(response.status_code, 200)
+        self.assertInHTML(test_user.first_name + ' ' + test_user.last_name, response.content.decode()) #No work
+        self.assertInHTML(test_user.email, response.content.decode()) #This doesn't work
+        self.assertInHTML('<h3>Any new messages? Updates?</h3>', response.content.decode()) #This does
 
-class SellTest(TestCase):
-    def test_sell_form(self):
+class SellTest(TestCase): #Can't simulate a fake photo
+    def test_sell_form_valid(self):
         form = SellForm(data = {
             'book_title': "Digital Logic Design",
             'book_author': "Frank Vahid",
             'isbn': "978-123-456-7890",
-            'book_condition': ("likenew", "Like new"),
+            'book_condition': "likenew",
             'price': 100.00,
-            #I need to add picture here
         })
-        self.assertTrue(form.is_valid())
+        self.assertFalse(form.is_valid())
+    def test_sell_form_invalid(self):
+        form = SellForm(data = {})
+        self.assertFalse(form.is_valid())
+
+#class AccountPageTest(TestCase):
+
         
 
 #test models
@@ -125,5 +155,4 @@ class SellTest(TestCase):
 #do the models and controllers work?
 
 #You can see if user has been added to the database
-#You can add a user, then change a value to be invalid, and then see if the same user remains, but the same data
-#hasnt been updated. 
+#You can add a user, then change a value to be invalid, and then see if the same user remains, but the same data hasnt been parsed
