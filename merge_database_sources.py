2f87aa398e454f997845728010a0602a4243edb2
Nick Winans <nw5zp@virginia.edu>
2020-04-05 15:59:24 -0400

    create titles for all classes

--- merge_database_sources.py
+++ merge_database_sources.py
@@ -6,11 +6,10 @@ with open('books_google_isbndb.json') as b:
             books = json.load(b)
             courses = json.load(c)['class_schedules']['records']
             for book in books:
-                if "**" not in book['Title'] and "(eBook" not in book['Title'] and "Access Card" not in book['Title'] and "access code" not in book['Title'].lower() and "Websam" not in book['Title']:
-                    for course in courses:
-                        if book['Dept'] == course[0] and book['Course'] == course[1]:
-                            book['ClassTitle'] = course[4]
-                            break
+                for course in courses:
+                    if book['Dept'] == course[0] and book['Course'] == course[1]:
+                        book['ClassTitle'] = course[4]
+                        break
         except:
             print('not json')
     with open('class_data.json', 'w+') as f:
