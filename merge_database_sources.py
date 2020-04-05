3fdd7491cf34eb8302efffdcb543a72abb043b72
Nick Winans <nw5zp@virginia.edu>
2020-04-05 16:10:03 -0400

    default class name = dept-coursecode

--- merge_database_sources.py
+++ merge_database_sources.py
@@ -2,7 +2,6 @@ import json
 
 with open('books_google_isbndb.json') as b:
     with open('uva_classes.json') as c:
-        try:
             books = json.load(b)
             courses = json.load(c)['class_schedules']['records']
             for book in books:
@@ -10,7 +9,7 @@ with open('books_google_isbndb.json') as b:
                     if book['Dept'] == course[0] and book['Course'] == course[1]:
                         book['ClassTitle'] = course[4]
                         break
-        except:
-            print('not json')
+                if not 'ClassTitle' in book:
+                    book['ClassTitle'] = book['Dept'] + "-" + book['Course']
     with open('class_data.json', 'w+') as f:
         json.dump(books, f, indent=4)
